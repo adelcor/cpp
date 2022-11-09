be698f07d8aaa2f6d5cd5769b0037ee771a7edbc
adel-cor <adel-cor@student.42urduliz.com>
2022-11-09 12:43:22 +0100

    fixed points improved

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -1,19 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adel-cor <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/26 17:26:28 by adel-cor          #+#    #+#             */
+/*   Updated: 2022/11/09 12:41:11 by adel-cor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Fixed.hpp"
 
 const int Fixed::_fract_bits = 8;
 
-Fixed::Fixed():_fp_value(0)
+Fixed::Fixed(void):_fp_value(0)
 {
 	std::cout << "Fixed Default Constructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed &copy)
+Fixed::Fixed(const Fixed &src)
 {
 	std::cout << "Fixed Copy Constructor called" << std::endl;
-	*this = copy;
+//	this->_fp_value = src._fp_value;
+	*this = src;
 }
 
-Fixed::~Fixed()
+Fixed::~Fixed(void)
 {
 	std::cout << "Fixed Deconstructor called" << std::endl;
 }
@@ -21,8 +34,7 @@ Fixed::~Fixed()
 Fixed &Fixed::operator=(const Fixed &src)
 {
 	std::cout << "Fixed Assignation operator called" << std::endl;
-	if(this != &src)
-		this->_fp_value = src.getRawBits();
+	this->_fp_value = src.getRawBits();
 	return *this;
 }
 
--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adel-cor <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/26 17:24:15 by adel-cor          #+#    #+#             */
+/*   Updated: 2022/11/09 12:41:08 by adel-cor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 
 class Fixed
@@ -6,9 +18,9 @@ class Fixed
 		int	_fp_value;
 		static const int _fract_bits;
 	public:
-		Fixed();
-		Fixed(const Fixed &copy);
-		~Fixed();
+		Fixed(void);
+		Fixed(const Fixed &src);
+		~Fixed(void);
 		Fixed &operator=(const Fixed &src);
 		int getRawBits(void)const;
 		void setRawBits(int const raw);
