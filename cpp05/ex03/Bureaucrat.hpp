0d28df89ac68732121ee3fa3ed21bad4619165ae
adel-cor <adel-cor@student.42urduliz.com>
2022-12-01 13:58:48 +0100

    added

---
+++ cpp05/ex03/Bureaucrat.hpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adel-cor <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/30 09:56:59 by adel-cor          #+#    #+#             */
+/*   Updated: 2022/12/01 11:21:29 by adel-cor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <string>
+#include <iostream>
+#include "Form.hpp"
+
+class Form;
+
+class Bureaucrat
+{
+	private:
+		const std::string _name;
+		size_t _grade;
+		void setGrade(int grade);
+	public:
+		Bureaucrat();
+		Bureaucrat(const Bureaucrat &src);
+		Bureaucrat(int grade);
+		Bureaucrat(const std::string name);
+		Bureaucrat(const std::string name, int grade);
+
+		~Bureaucrat();
+
+		Bureaucrat &operator=(const Bureaucrat &src);
+
+		void incrementGrade(void);
+		void decrementGrade(void);
+		void signForm(Form &form);
+		void executeForm(Form &form)const;
+		const std::string getName(void)const;
+		size_t getGrade(void)const;
+
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		virtual const char *what() const throw();
+	};
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		virtual const char  *what() const throw();
+	};
+};
+
+std::ostream &operator<<(std::ostream &o, Bureaucrat *a);
+
+#endif
+
+
+
+
---
+++ cpp05/ex03/Intern.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Intern.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adel-cor <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/01 13:26:30 by adel-cor          #+#    #+#             */
+/*   Updated: 2022/12/01 13:51:51 by adel-cor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma	once
+#include <string>
+#include <iostream>
+#include "Form.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+
+class Form;
+class PresidentialPardonForm;
+class RobotomyRequestForm;
+class ShrubberyCreationForm;
+
+class Intern
+{
+	private:
+
+	public:
+		Intern();
+		Intern(const Intern &src);
+
+		~Intern();
+
+		Intern &operator=(const Intern &src);
+
+		Form *makeForm(const std::string form, const std::string target);
+
+};
