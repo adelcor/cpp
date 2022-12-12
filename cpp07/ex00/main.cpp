96ea083fb2035f9b47f43dc709536821561e7d96
adel-cor <adel-cor@student.42urduliz.com>
2022-12-12 17:38:08 +0100

    added ex00

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adel-cor <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/12 17:24:04 by adel-cor          #+#    #+#             */
+/*   Updated: 2022/12/12 17:35:26 by adel-cor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+#include <iostream>
+
+int	main()
+{
+
+	{
+	float	a = 21.445f;
+	float	b = 42.445f;
+
+	std::cout << "The max of a: " << a << " and b: " << b << " is:" <<
+	"\t" << max(a, b) << std::endl;
+
+	std::cout << "The min of a: " << a << " and b: " << b << " is:" <<
+	"\t" << min(a, b) << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "before:\n\ta: " << a << "\n\tb: " << b << std::endl;
+	swap(a, b);
+	std::cout << "after:\n\ta: " << a << "\n\tb: " << b << std::endl;
+	}
+
+	std::cout << std::endl << "**TEST**TEST**TEST**42**42**TEST**TEST**TEST**" << std::endl << std::endl;
+
+	{
+	std::string	a = "Terminator";
+	std::string b = "Gandalf";
+
+	std::cout << "The max of a: " << a << " and b: " << b << " is:" <<
+	"\t" << ::max(a, b) << std::endl;
+
+	std::cout << "The min of a: " << a << " and b: " << b << " is:" <<
+	"\t" << ::min(a, b) << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "before:\n\ta: " << a << "\n\tb: " << b << std::endl;
+	::swap(a, b);
+	std::cout << "after:\n\ta: " << a << "\n\tb: " << b << std::endl;
+	}
+
+	
+
+
+	return (0);
+}
---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adel-cor <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/12 17:13:41 by adel-cor          #+#    #+#             */
+/*   Updated: 2022/12/12 17:37:04 by adel-cor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_H
+#define WHATEVER_H
+
+template <typename T>
+
+const T &max(const T &x, const T &y)
+{
+	return(x >= y ? x : y);
+}
+
+template <typename C>
+
+const C &min(const C &x, const C &y)
+{
+		return(x <= y ? x : y);
+}
+
+template <typename M>
+
+void swap(M &x, M &y)
+{
+	M temp = x;
+	x = y;
+	y = temp;
+}
+#endif
+
